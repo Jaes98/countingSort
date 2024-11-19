c2852085666a0908e0fd6603f06556cf08d7b0a6
sassnoe <105166994+sassnoe@users.noreply.github.com>
2024-11-19 11:35:02 +0100

    Midlertidigt virkende countingSort

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-window.addEventListener('load', start)
+window.addEventListener("load", start);
+
+const arr = [1, 4, 1, 2, 7, 5, 2];
 
 function start() {
-    console.log('JS running');
+  console.log("JS running");
+  console.log("Original array is: ", arr);
+  console.log("Sorted array is: ", countingSort(arr));
 }
 
-function countingSort(params) {
-    
+function countingSort(arr) {
+  const max = Math.max(...arr);
+  const countArr = Array(max + 1);
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!countArr[arr[i]]) {
+      countArr[arr[i]] = 0;
+    }
+
+    countArr[arr[i]]++;
+  }
+
+  for (let i = 0; i < countArr.length; i++) {
+    while (countArr[i] > 0) {
+      result.push(i);
+      countArr[i]--;
+    }
+  }
+
+  console.log(result);
+
+  return result;
 }
